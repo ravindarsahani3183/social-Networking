6b48fe37a7aeb3ca5d62a8f391605a5ba2b51ea2
ravindarsahani3183 <ravindarsahani3183@gmail.com>
2023-09-06 22:00:19 +0530

    delete account

--- src/Setting/SettingSidebar.js
+++ src/Setting/SettingSidebar.js
@@ -4,6 +4,9 @@ import { Link } from 'react-router-dom';
 import { FcBusinessman } from 'react-icons/fc';
 import { IoIosNotificationsOutline } from 'react-icons/io';
 import { AiOutlineSafetyCertificate} from 'react-icons/ai';
+import { PiHandshake} from 'react-icons/pi';
+import { GoDiscussionClosed} from 'react-icons/go';
+import {RiDeleteBin6Line} from 'react-icons/ri';
 
 const SettingSidebar = () => {
   return (
@@ -13,9 +16,9 @@ const SettingSidebar = () => {
             <li className='setting-sidebar-li'><Link className='setting-sidebar-list fs-6' to="/SettingSidebar"><FcBusinessman/>Account</Link></li>
             <li className='setting-sidebar-li'><Link className='setting-sidebar-list fs-6' to="/SettingSidebar"><IoIosNotificationsOutline/>Notification</Link></li>
             <li className='setting-sidebar-li'><Link className='setting-sidebar-list fs-6' to="/SettingSidebar"><AiOutlineSafetyCertificate/>privacy and safety</Link></li>
-            <li className='setting-sidebar-li'><Link className='setting-sidebar-list fs-6' to="/SettingSidebar">communications</Link></li>
-            <li className='setting-sidebar-li'><Link className='setting-sidebar-list fs-6' to="/SettingSidebar">messaging</Link></li>
-            <li className='setting-sidebar-li'><Link className='setting-sidebar-list fs-6' to="/SettingSidebar">close account</Link></li>
+            <li className='setting-sidebar-li'><Link className='setting-sidebar-list fs-6' to="/SettingSidebar"><PiHandshake/>communications</Link></li>
+            <li className='setting-sidebar-li'><Link className='setting-sidebar-list fs-6' to="/SettingSidebar"><GoDiscussionClosed/>messaging</Link></li>
+            <li className='setting-sidebar-li'><Link className='setting-sidebar-list fs-6' to="/SettingSidebar"><RiDeleteBin6Line/>close account</Link></li>
         </ul>
       </div>
     </div>

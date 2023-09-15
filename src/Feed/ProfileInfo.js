653c5b91eba847d2221ecc05cabef42836309c50
ravindarsahani3183 <ravindarsahani3183@gmail.com>
2023-09-15 22:21:53 +0530

    add about component

--- src/Feed/ProfileInfo.js
+++ src/Feed/ProfileInfo.js
@@ -2,9 +2,22 @@ import React from 'react'
 import './ProfileInfo.css';
 const ProfileInfo = () => {
   return (
-    <div className='ProfileForm'>
-      hii my namy is ravindar
-      i am going to delhi
+    <div>
+      <div className='ProfileForm'>
+      <h5 className='about-profile-heading'>Profile Info</h5>
+       <div className='ProfileForm-Overview'>
+        <h6>Overview</h6>
+        <p>He moonlights difficult engrossed it, sportsmen. 
+          Interested has all Devonshire difficulty gay assistance joy. 
+          Handsome met debating sir dwelling age material.
+          As style lived he worse dried. Offered related so visitors we private removed. 
+          Moderate do subjects to distance.
+        </p>
+       </div>
+       <form>
+        
+       </form>
+      </div>
     </div>
   )
 }

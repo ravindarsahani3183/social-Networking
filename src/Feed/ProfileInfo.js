a7ca09e02c64b98061a971f39a981e06bb57cbdb
ravindarsahani3183 <ravindarsahani3183@gmail.com>
2023-09-16 23:49:09 +0530

    update profile info

--- src/Feed/ProfileInfo.js
+++ src/Feed/ProfileInfo.js
@@ -6,17 +6,47 @@ const ProfileInfo = () => {
       <div className='ProfileForm'>
       <h5 className='about-profile-heading'>Profile Info</h5>
        <div className='ProfileForm-Overview'>
-        <h6>Overview</h6>
-        <p>He moonlights difficult engrossed it, sportsmen. 
+        <h6 className='ProfileForm-Overview-heading'>Overview</h6>
+        <p className='ProfileForm-Overview-para'>He moonlights difficult engrossed it, sportsmen. 
           Interested has all Devonshire difficulty gay assistance joy. 
           Handsome met debating sir dwelling age material.
           As style lived he worse dried. Offered related so visitors we private removed. 
           Moderate do subjects to distance.
         </p>
        </div>
-       <form>
-        
-       </form>
+       <div className='about-profile-info'>
+        <div className='rounded border about-profile-input'>
+          <p className='about-profile-info2'>born <strong className='about-info-strong'>oct 12 2000</strong></p>
+          <strong className='about-profile-dropdown'>...</strong>
+        </div>
+        <div className='rounded border about-profile-input'>
+          <p className='about-profile-info2'>born <strong className='about-info-strong'>oct 12 2000</strong></p>
+          <strong className='about-profile-dropdown'>...</strong>
+        </div>
+       </div>
+
+       <div className='about-profile-info'>
+        <div className='rounded border about-profile-input'>
+          <p className='about-profile-info2'>born <strong className='about-info-strong'>oct 12 2000</strong></p>
+          <strong className='about-profile-dropdown'>...</strong>
+        </div>
+        <div className='rounded border about-profile-input'>
+          <p className='about-profile-info2'>born <strong className='about-info-strong'>oct 12 2000</strong></p>
+          <strong className='about-profile-dropdown'>...</strong>
+        </div>
+       </div>
+
+       <div className='about-profile-info'>
+        <div className='rounded border about-profile-input'>
+          <p className='about-profile-info2'>born <strong className='about-info-strong'>oct 12 2000</strong></p>
+          <strong className='about-profile-dropdown'>...</strong>
+        </div>
+        <div className='rounded border about-profile-input'>
+          <p className='about-profile-info2'>born <strong className='about-info-strong'>oct 12 2000</strong></p>
+          <strong className='about-profile-dropdown'>...</strong>
+        </div>
+       </div>
+
       </div>
     </div>
   )
